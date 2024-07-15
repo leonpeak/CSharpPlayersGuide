6cd2fbde52ca363547a46792bdeb4f9b4d914d82
Leon Peak <71638912+leonpeak@users.noreply.github.com>
2024-07-15 22:03:10 +0100

    Create simple calculator program utilising Swtich statements

---
+++ Example_Exercises/Chapter_12/Simple_Calculator/Simple_Calculator/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Simple_Calculator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please enter your first number: ");
+        string num1Input = Console.ReadLine();
+        int num1 = int.Parse(num1Input);
+
+        Console.Write("Please enter your second number: ");
+        string num2Input = Console.ReadLine();
+        int num2 = int.Parse(num2Input);
+
+        Console.Write("What would you like to do with your numbers? Enter +,-,%,x,^ to choose your operation: ");
+        string userOperation = Console.ReadLine();
+
+        switch (userOperation)
+        {
+            case "+":
+            Console.WriteLine(num1 + num2);
+            break;
+            case "-":
+            Console.WriteLine(num1 - num2);
+            break;
+            case "%":
+            Console.WriteLine(num1 / num2);
+            break;  
+            case "x":
+            Console.WriteLine(num1 * num2);
+            break;
+            case "^":
+            Console.WriteLine(Math.Pow(num1, num2));
+            break;
+            default: 
+            Console.WriteLine("That is not an operator, please try again!"); 
+            break;  
+        }
+
+    }
+}
